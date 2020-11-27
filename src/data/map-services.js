c05b39285b8c3f9d51214e603cb4c452ccc0db0d
allezz <aerbay@gmail.com>
2020-11-27 16:24:08 +0100

    Change env variable

--- src/data/map-services.js
+++ src/data/map-services.js
@@ -1,18 +1,19 @@
-const accessToken = process.env.REACT_APP_MAPKEY;
+const accessToken = process.env.GATSBY_APP_MAPKEY;
 export const mapServices = [
   {
-    name: 'OpenStreetMap',
-    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
-    url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+    name: "OpenStreetMap",
+    attribution:
+      '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+    url: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
   },
   {
-    name: 'BlueMarble',
-    attribution: '&copy; NASA Blue Marble, image service by OpenGeo',
+    name: "BlueMarble",
+    attribution: "&copy; NASA Blue Marble, image service by OpenGeo",
     url:
-      'https://gibs-{s}.earthdata.nasa.gov/wmts/epsg3857/best/BlueMarble_ShadedRelief_Bathymetry/default//EPSG3857_500m/{z}/{y}/{x}.jpeg',
+      "https://gibs-{s}.earthdata.nasa.gov/wmts/epsg3857/best/BlueMarble_ShadedRelief_Bathymetry/default//EPSG3857_500m/{z}/{y}/{x}.jpeg",
   },
   {
-    name: 'Mapbox',
+    name: "Mapbox",
     attribution:
       'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     url: `https://api.mapbox.com/styles/v1/allezz/ckhpdoe4801mm19rz9jrjpge1/tiles/256/{z}/{x}/{y}@2x?access_token=${accessToken}`,
